3f6f711361b1c3f31b4dc386b1b72238ce019e6a
codebrain001 <abozejohn@gmail.com>
2020-12-29 18:08:50 +0100

    Got the contact list from whatsapp

--- automation.py
+++ automation.py
@@ -1,4 +1,20 @@
+import time
 from selenium import webdriver
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-chrome_browser.get('https://www.seleniumeasy.com/test/')
+chrome_browser.maximize_window()
+chrome_browser.get('https://web.whatsapp.com/')
+
+time.sleep(45)
+print('Scanning the QR code completed')
+
+# Finding the Whatsapp Group
+group_name = 'PortHarcourt School of AI'
+group = chrome_browser.find_element_by_class_name('_1MZWu')
+print('About to click...')
+group.click()
+
+
+contacts = chrome_browser.find_element_by_xpath(
+    '//*[@id="main"]/header/div[2]/div[2]/span')
+print(contacts.get_attribute('textContent'))
