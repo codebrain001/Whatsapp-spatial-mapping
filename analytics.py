26450258150606765dfa0c81b8f324c55ec3cb95
codebrain001 <abozejohn@gmail.com>
2020-12-30 12:31:26 +0100

    Framework for analytic script

---
+++ analytics.py
@@ -0,0 +1,9 @@
+# Set up all the enivronment variables
+
+# Get random sample of the data
+
+# Geo encode: Geocoder
+
+# Set up Mapbox
+
+# Plot the spatial map
--- try.py
+++ try.py
@@ -1,7 +1,11 @@
-import nexmo
-from pprint import pprint
+# import nexmo
+# from pprint import pprint
 
-client = nexmo.Client(key='31a8b23d', secret='XLu6QzbdWFuvEyyy')
+# client = nexmo.Client(key='31a8b23d', secret='XLu6QzbdWFuvEyyy')
 
-insight_json = client.get_advanced_number_insight(number='2348141921557')
-pprint(insight_json)
+# insight_json = client.get_advanced_number_insight(number='2348141921557')
+# pprint(insight_json)
+
+import geocoder
+g = geocoder.google('Nigeria')
+print(g.latlng)
