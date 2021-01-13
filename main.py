f89366ec64b9447edf903527324de7935a220e26
codebrain001 <abozejohn@gmail.com>
2021-01-13 22:40:31 +0100

    Refactored main script

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from analytics import WhatsappAnalytics
 from geocoding import GoogleGeocoding
 from plotting import SpatialMapping
 
-automated_object = WhatsappAutomation()
-group_xpath = '//*[@id="pane-side"]/div[1]/div/div'
-contact_xpath = '//*[@id="main"]/header/div[2]/div[2]/span'
-contact_list = automated_object.get_contacts(group_xpath, contact_xpath)
-automated_object.quit()
+if __name__ == '__main__':
 
-analytics_object = WhatsappAnalytics()
-analytics_df = analytics_object.get_insights(contact_list[50:100])
+    automated_object = WhatsappAutomation()
+    group_xpath = '//*[@id="pane-side"]/div[1]/div/div'
+    contact_xpath = '//*[@id="main"]/header/div[2]/div[2]/span'
+    contact_list = automated_object.get_contacts(group_xpath, contact_xpath)
+    automated_object.quit()
 
-geocoding_object = GoogleGeocoding()
-geo_df = geocoding_object.geocode_df(analytics_df)
+    analytics_object = WhatsappAnalytics()
+    analytics_df = analytics_object.get_insights(contact_list)
 
-spatial_mapping_object = SpatialMapping()
-spatial_mapping_object.plot_map(geo_df)
-spatial_mapping_object.plot_bar(geo_df)
+    geocoding_object = GoogleGeocoding()
+    geo_df = geocoding_object.geocode_df(analytics_df)
+
+    spatial_mapping_object = SpatialMapping()
+    spatial_mapping_object.plot_map(geo_df)
+    spatial_mapping_object.plot_bar(geo_df)
